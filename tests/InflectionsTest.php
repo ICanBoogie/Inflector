e6d7d45ec102a3774fa647f0ac1fcc2dcceebffe
Peter Scopes <peter.scopes@nccgroup.trust>
2019-04-25 09:50:05 +0100

    Adds tests for is_singular and is_plural

--- tests/InflectionsTest.php
+++ tests/InflectionsTest.php
@@ -32,6 +32,45 @@ class InflectionsTest extends \PHPUnit_Framework_TestCase
 	}
 
 	/**
+	 * @dataProvider provide_singular_and_plural
+	 *
+	 * @param string $locale
+	 * @param string $singular
+	 * @param string $plural
+	 */
+	public function test_is_singular($locale, $singular, $plural)
+	{
+		$matched = preg_match('/\b[[:word:]]+\Z/u', downcase($singular), $matches);
+		if ($matched && isset(Inflector::get($locale)->inflections->uncountables[$matches[0]])) {
+			$this->assertTrue(Inflector::get($locale)->is_uncountable($singular));
+			$this->assertTrue(Inflector::get($locale)->is_uncountable($plural));
+		} else {
+			$this->assertTrue(Inflector::get($locale)->is_singular($singular));
+			$this->assertFalse($singular != $plural && Inflector::get($locale)->is_singular($plural));
+		}
+	}
+
+	/**
+	 * @dataProvider provide_singular_and_plural
+	 *
+	 * @param string $locale
+	 * @param string $singular
+	 * @param string $plural
+	 */
+	public function test_is_plural($locale, $singular, $plural)
+	{
+		$matched = preg_match('/\b[[:word:]]+\Z/u', downcase($singular), $matches);
+		if (!$matched) var_dump($matches);
+		if ($matched && isset(Inflector::get($locale)->inflections->uncountables[$matches[0]])) {
+			$this->assertTrue(Inflector::get($locale)->is_uncountable($singular));
+			$this->assertTrue(Inflector::get($locale)->is_uncountable($plural));
+		} else {
+			$this->assertTrue(Inflector::get($locale)->is_plural($plural));
+			$this->assertFalse($singular != $plural && Inflector::get($locale)->is_plural($singular));
+		}
+	}
+
+	/**
 	 * @return array
 	 */
 	public function provide_singular_and_plural()
